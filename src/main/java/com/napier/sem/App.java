be08ae35d1037b0f541dd200490dfbc15d1b398b
Dave Lake <dclake@gmail.com>
2021-02-13 16:14:12 -0400

    Closed issue #7 - Display employee information

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -4,73 +4,73 @@ import java.sql.*;
 
 public class App
 {
-        /**
-         * Connection to MySQL database.
-         */
-        private Connection con = null;
+    /**
+     * Connection to MySQL database.
+     */
+    private Connection con = null;
 
-        /**
-         * Connect to the MySQL database.
-         */
-        public void connect()
+    /**
+     * Connect to the MySQL database.
+     */
+    public void connect()
+    {
+        try
+        {
+            // Load Database driver
+            Class.forName("com.mysql.jdbc.Driver");
+        }
+        catch (ClassNotFoundException e)
+        {
+            System.out.println("Could not load SQL driver");
+            System.exit(-1);
+        }
+
+        int retries = 10;
+        for (int i = 0; i < retries; ++i)
+        {
+            System.out.println("Connecting to database...");
+            try
+            {
+                // Wait a bit for db to start
+                Thread.sleep(30000);
+                // Connect to  local database
+                //con = DriverManager.getConnection("jdbc:mysql://localhost:33060/employees?useSSL=false", "root", "example");
+                // Connect to database inside docker
+                con = DriverManager.getConnection("jdbc:mysql://db:3306/employees?useSSL=false", "root", "example");
+                System.out.println("Successfully connected");
+                break;
+            }
+            catch (SQLException sqle)
+            {
+                System.out.println("Failed to connect to database attempt " + Integer.toString(i));
+                System.out.println(sqle.getMessage());
+            }
+            catch (InterruptedException ie)
+            {
+                System.out.println("Thread interrupted? Should not happen.");
+            }
+        }
+    }
+
+    /**
+     * Disconnect from the MySQL database.
+     */
+    public void disconnect()
+    {
+        if (con != null)
         {
             try
             {
-                // Load Database driver
-                Class.forName("com.mysql.jdbc.Driver");
+                // Close connection
+                con.close();
             }
-            catch (ClassNotFoundException e)
+            catch (Exception e)
             {
-                System.out.println("Could not load SQL driver");
-                System.exit(-1);
-            }
-
-            int retries = 10;
-            for (int i = 0; i < retries; ++i)
-            {
-                System.out.println("Connecting to database...");
-                try
-                {
-                    // Wait a bit for db to start
-                    Thread.sleep(30000);
-                    // Connect to  local database
-                    con = DriverManager.getConnection("jdbc:mysql://localhost:33060/employees?useSSL=false", "root", "example");
-                    // Connect to database inside docker
-                   // con = DriverManager.getConnection("jdbc:mysql://db:3306/employees?useSSL=false", "root", "example");
-                    System.out.println("Successfully connected");
-                    break;
-                }
-                catch (SQLException sqle)
-                {
-                    System.out.println("Failed to connect to database attempt " + Integer.toString(i));
-                    System.out.println(sqle.getMessage());
-                }
-                catch (InterruptedException ie)
-                {
-                    System.out.println("Thread interrupted? Should not happen.");
-                }
+                System.out.println("Error closing connection to database");
             }
         }
 
-        /**
-         * Disconnect from the MySQL database.
-         */
-        public void disconnect()
-        {
-            if (con != null)
-            {
-                try
-                {
-                    // Close connection
-                    con.close();
-                }
-                catch (Exception e)
-                {
-                    System.out.println("Error closing connection to database");
-                }
-            }
-
-        }
+    }
     public Employee getEmployee(int ID)
     {
         try
@@ -80,12 +80,14 @@ public class App
             // Create string for SQL statement
             String strSelect =
                     "SELECT DISTINCT employees.emp_no, employees.first_name, employees.last_name, titles.title, "
-                           + "salaries.salary, departments.dept_name "
-                           + "FROM employees, titles, salaries, dept_emp, departments "
-                           + "WHERE employees.emp_no = titles.emp_no AND employees.emp_no = salaries.emp_no "
-                           + "AND employees.emp_no = dept_emp.emp_no AND dept_emp.dept_no = departments.dept_no "
-                           + "AND salaries.to_date = '9999-01-01' "
-                           + "AND employees.emp_no = " + ID;
+                            + "salaries.salary, departments.dept_name "
+                            + "FROM employees, titles, salaries, dept_emp, departments "
+                            + "WHERE employees.emp_no = titles.emp_no AND employees.emp_no = salaries.emp_no "
+                            + "AND employees.emp_no = dept_emp.emp_no AND dept_emp.dept_no = departments.dept_no "
+                            + "AND salaries.to_date = '9999-01-01' "
+                            + "AND employees.emp_no = " + ID;
+
+
 
             // Execute SQL statement
             ResultSet rset = stmt.executeQuery(strSelect);
