d1ec1ad7abe9dee79fe0ae37008fe68ace3ba667
dave.lake <dclake@gmail.com>
2021-03-30 15:24:09 -0400

    Created more tests

---
+++ src/test/java/com/napier/sem/AppTest.java
@@ -0,0 +1,30 @@
+package com.napier.sem;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+public class AppTest
+{
+    static App app;
+
+    @BeforeAll
+    static void init()
+    {
+        app = new App();
+    }
+
+    @Test
+    void printSalariesTestNull()
+    {
+        app.printSalaries(null);
+    }
+    @Test
+    void printSalariesTestEmpty()
+    {
+        ArrayList<Employee> employess = new ArrayList<Employee>();
+        app.printSalaries(employess);
+    }
+    
+}
